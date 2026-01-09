e67419abb24f2853ed23be9cb229731dbe329e3e
Larry Kollar <lkollar@gmail.com>
2026-01-08 21:23:40 -0500

    Fixed INSTALL.sh so it works now.

--- INSTALL.sh
+++ INSTALL.sh
@@ -8,10 +8,11 @@ fi
 
 # if XDG_DATA_HOME does not exist,
 # default Pandoc install dir is $HOME/.local/share/pandoc
-pandoc_scripts = $HOME/.local/share/pandoc
-if [ -v XDG_DATA_HOME ]; then
-  pandoc_scripts = $XDG_DATA_HOME/pandoc;
+pandoc_scripts=$HOME/.local/share/pandoc
+if [ ! -z ${XDG_DATA_HOME+x} ]; then
+  pandoc_scripts=$XDG_DATA_HOME/pandoc;
 fi
+echo Pandoc scripts will be copied to ${pandoc_scripts}.
 
 # copy default.html and vars.lua
 mkdir -p ${pandoc_scripts}/filters ${pandoc_scripts}/templates
@@ -33,11 +34,11 @@ echo $PATH | grep -q "$HOME/bin:" || (
 
 # check for presence of Pandoc and PDF formatters
 # print links for each if needed
-which pandoc || (
+which -s pandoc || (
   echo "Pandoc is required for the Humanist Publishing System."
   echo "See <https://pandoc.org/installing.html> for installion info."
 )
-which weasyprint || (
+which -s weasyprint || (
   echo "weasyprint is the default PDF formatter <weasyprint.org>."
   echo "You can use a different formatter; see the Pandoc docs"
   echo "<https://pandoc.org/MANUAL.html#option--pdf-engine> for details."
