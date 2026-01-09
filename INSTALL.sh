86cc10735a698a9602cc356ad1d44bac1b4d01fb
Larry Kollar <lkollar@gmail.com>
2026-01-09 15:46:03 -0500

    Fixed several INSTALL.sh issues.

--- INSTALL.sh
+++ INSTALL.sh
@@ -8,10 +8,11 @@ fi
 
 # if XDG_DATA_HOME does not exist,
 # default Pandoc install dir is $HOME/.local/share/pandoc
-pandoc_scripts = $HOME/.local/share/pandoc
-if [ -v XDG_DATA_HOME ]; then
-  pandoc_scripts = $XDG_DATA_HOME/pandoc;
+pandoc_scripts=$HOME/.local/share/pandoc
+if [ ! -z ${XDG_DATA_HOME+x} ]; then
+  pandoc_scripts=$XDG_DATA_HOME/pandoc;
 fi
+echo Pandoc scripts will be copied to ${pandoc_scripts}.
 
 # copy default.html and vars.lua
 mkdir -p ${pandoc_scripts}/filters ${pandoc_scripts}/templates
@@ -25,7 +26,7 @@ chmod +x $HOME/bin/humanist $HOME/bin/stitch
 
 # make sure $PATH includes ~/bin
 echo $PATH | grep -q "$HOME/bin:" || (
-  echo -n "# $HOME/bin added to PATH by Humanist install: " >> $HOME/.profile
+  printf "# $HOME/bin added to PATH by Humanist install: " >> $HOME/.profile
   date >>$HOME/.profile
   echo "export PATH=${HOME}/bin:${PATH}" >> $HOME/.profile
   export PATH=${HOME}/bin:${PATH} # so we can use it right away
@@ -33,11 +34,11 @@ echo $PATH | grep -q "$HOME/bin:" || (
 
 # check for presence of Pandoc and PDF formatters
 # print links for each if needed
-which pandoc || (
+which -s pandoc || (
   echo "Pandoc is required for the Humanist Publishing System."
   echo "See <https://pandoc.org/installing.html> for installion info."
 )
-which weasyprint || (
+which -s weasyprint || (
   echo "weasyprint is the default PDF formatter <weasyprint.org>."
   echo "You can use a different formatter; see the Pandoc docs"
   echo "<https://pandoc.org/MANUAL.html#option--pdf-engine> for details."
